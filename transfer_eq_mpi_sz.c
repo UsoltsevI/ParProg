361d156275bf3791d2605545fbb9b99dd4de4096
UsoltsevI <usoltsev.ia@phystech.edu>
2026-04-06 12:48:53 +0300

    Remove mem leaks; Fix max tag value error

--- transfer_eq_mpi_sz.c
+++ transfer_eq_mpi_sz.c
@@ -1,5 +1,7 @@
 // 
 // Решение задачи переноса параллельным алгоритмом
+// 
+// оптимизированный алгоритм: один процесс только под сборку данных
 //
 
 #include <stdio.h>
@@ -141,7 +143,7 @@ void out_like_csv(FILE* out, double** u, int K, int M) {
 }
 
 int calc_tag_uk(int k, int part, int size) {
-    return k + part;
+    return abs(k + part);
 }
 
 int get_prev_rank(int rank, int size) {
@@ -156,7 +158,7 @@ int get_next_rank(int rank, int size) {
  * tag для отправки всего ряда нулевому процессу для сборки
  */
 int calc_root_tag_uk(int k) {
-    return 32000 - k; // 32767
+    return abs(32000 - k); // 32767
 }
 
 // DEBUG
@@ -169,7 +171,7 @@ void print_double_arr(double* arr, int size) {
 /**
  * Отправить данные о части процессу
  */
-void send_uk_data(double* uk, double* uk1, int start, int end, int to_rank, int tag, int M) {
+double* send_uk_data(double* uk, double* uk1, int start, int end, int to_rank, int tag, int M, MPI_Request* request) {
     // отправляем с следующем формате:
     // из tag сразу можно дстать k и part
     // поэтому отпрвляем чисто uk и uk1 подряд
@@ -177,11 +179,8 @@ void send_uk_data(double* uk, double* uk1, int start, int end, int to_rank, int 
     double* ukuk = (double *) calloc(sz * 2, sizeof(double));
     memcpy(ukuk, uk + start, sz * sizeof(double));
     memcpy(ukuk + sz, uk1 + start, sz * sizeof(double));
-
-    MPI_Request request;
-    
-    MPI_Isend(ukuk, sz * 2, MPI_DOUBLE, to_rank, tag, MPI_COMM_WORLD, &request);
-    // free(ukuk); пока с утечкой
+    MPI_Isend(ukuk, sz * 2, MPI_DOUBLE, to_rank, tag, MPI_COMM_WORLD, request);
+    return ukuk;
 }
 
 /**
@@ -194,19 +193,17 @@ void recv_uk_data(double* uk, double* uk1, int start, int end, int from_rank, in
     MPI_Recv(buf, sz * 2, MPI_DOUBLE, from_rank, tag, MPI_COMM_WORLD, &status);
     memcpy(uk + start, buf, sz * sizeof(double));
     memcpy(uk1 + start, buf + sz, sz * sizeof(double));
-
     free(buf);
 }
 
 /**
  * Отправить результаты на 0 процесс
  */
-void send_uk_to_root(double* uk, int k, int M, int root_rank) {
+double* send_uk_to_root(double* uk, int k, int M, int root_rank, MPI_Request* request) {
     double* copy = (double *) malloc(M * sizeof(double));
     memcpy(copy, uk, M * sizeof(double));
-    MPI_Request request;
-    MPI_Isend(copy, M, MPI_DOUBLE, root_rank, calc_root_tag_uk(k), MPI_COMM_WORLD, &request);
-    // free(copy); пока с утечкой
+    MPI_Isend(copy, M, MPI_DOUBLE, root_rank, calc_root_tag_uk(k), MPI_COMM_WORLD, request);
+    return copy;
 }
 
 /**
@@ -269,6 +266,24 @@ int main(int argc, char** argv) {
     double* uk_next = (double *) calloc(M, sizeof(double));
     double* phi = NULL;
 
+    
+    MPI_Request* requests = NULL;
+    double** send_buffers = NULL;
+    if (rank != size) {
+        int max_requests = K / size + K + size;
+        requests = (MPI_Request*) calloc(max_requests, sizeof(MPI_Request));
+        if (requests == NULL) {
+            fprintf(stderr, "Ошибка выделения памяти: %s\n", strerror(errno));
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        send_buffers = (double**) calloc(max_requests, sizeof(double*));
+        if (send_buffers == NULL) {
+            fprintf(stderr, "Ошибка выделения памяти: %s\n", strerror(errno));
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+    }
+    size_t last_req = 0;
+
     // root только собирает данные
     // остальные считают
 
@@ -289,60 +304,68 @@ int main(int argc, char** argv) {
             for (int m = MAX(start, 1); m < MIN(end, M - 1); m++) {
                 uk_next[m] = calc_center_three(phi, fk, m, tau, h);
             }
-            send_uk_data(uk_next, phi, start, end, get_next_rank(rank, size), calc_tag_uk(1, part, size), M);
+            send_buffers[last_req] = send_uk_data(uk_next, phi, start, end, get_next_rank(rank, size), 
+                calc_tag_uk(1, part, size), M, &requests[last_req]);
+            last_req++;
         }
 
-        send_uk_to_root(uk_next, 1, M, size);
+        send_buffers[last_req] = send_uk_to_root(uk_next, 1, M, size, &requests[last_req]);
+        last_req++;
         free(phi);
     }
 
     if (rank < size) {
         // Мы считаем для k + 1
-    for (int k = rank; k < K; k += size) {
-        if (k == 0) {
-            continue; // Первый шаг уже сделали
-        }
-        assert(k != 0);
-        get_fk(fk, k, M);
-        uk_next[0] = get_psi(k + 1);
+        for (int k = rank; k < K; k += size) {
+            if (k == 0) {
+                continue; // Первый шаг уже сделали
+            }
+            assert(k != 0);
+            get_fk(fk, k, M);
+            uk_next[0] = get_psi(k + 1);
 
-        for (int part = 0; part < size; part++) {
-            // Получаем данные от k - 1 процесса
-            int start = steps[part];
-            int end = steps[part + 1];
-            recv_uk_data(uk, uk1, start, end, get_prev_rank(rank, size), calc_tag_uk(k, part, size), M);
+            for (int part = 0; part < size; part++) {
+                // Получаем данные от k - 1 процесса
+                int start = steps[part];
+                int end = steps[part + 1];
+                recv_uk_data(uk, uk1, start, end, get_prev_rank(rank, size), calc_tag_uk(k, part, size), M);
 
-            if (start > 0) {
-                // читаем первый элемент
-                uk_next[start - 1] = calc_cross(uk, uk1, fk, start - 1, tau, h);
-                if (k < K - 1) {
-                    int prev_start = steps[part - 1];
-                    int prev_end = steps[part];
-                    assert(prev_end == start);
-                    // отправка (part - 1) !!!
-                    send_uk_data(uk_next, uk, prev_start, prev_end,  get_next_rank(rank, size), calc_tag_uk(k + 1, part - 1, size), M);
+                if (start > 0) {
+                    // читаем первый элемент
+                    uk_next[start - 1] = calc_cross(uk, uk1, fk, start - 1, tau, h);
+                    if (k < K - 1) {
+                        int prev_start = steps[part - 1];
+                        int prev_end = steps[part];
+                        assert(prev_end == start);
+                        // отправка (part - 1) !!!
+                        send_buffers[last_req] = send_uk_data(uk_next, uk, prev_start, prev_end, 
+                            get_next_rank(rank, size), calc_tag_uk(k + 1, part - 1, size), M, &requests[last_req]);
+                        last_req++;
+                    }
+                }
+
+                for (int m = MAX(start, 1); m < end - 1; m++) {
+                    // следующий элемент посчитать не можем, так как не достаточно данных
+                    uk_next[m] = calc_cross(uk, uk1, fk, m, tau, h);
+                }
+                if (part == size - 1) {
+                    assert(end - 1 == M - 1);
+                    uk_next[M - 1] = calc_corner(uk, fk, M - 1, tau, h);
+                }
+                if (part == size - 1) {
+                    assert(end == M);
+                }
+                if (end == M && k < K - 1) {
+                    assert(part == size - 1);
+                    send_buffers[last_req] = send_uk_data(uk_next, uk, start, end, get_next_rank(rank, size), 
+                        calc_tag_uk(k + 1, part, size), M, &requests[last_req]);
+                    last_req++;
                 }
             }
 
-            for (int m = MAX(start, 1); m < end - 1; m++) {
-                // следующий элемент посчитать не можем, так как не достаточно данных
-                uk_next[m] = calc_cross(uk, uk1, fk, m, tau, h);
-            }
-            if (part == size - 1) {
-                assert(end - 1 == M - 1);
-                uk_next[M - 1] = calc_corner(uk, fk, M - 1, tau, h);
-            }
-            if (part == size - 1) {
-                assert(end == M);
-            }
-            if (end == M && k < K - 1) {
-                assert(part == size - 1);
-                send_uk_data(uk_next, uk, start, end, get_next_rank(rank, size), calc_tag_uk(k + 1, part, size), M);
-            }
+            send_buffers[last_req] = send_uk_to_root(uk_next, k + 1, M, size, &requests[last_req]);
+            last_req++;
         }
-
-        send_uk_to_root(uk_next, k + 1, M, size);
-    }
     }
 
     free(fk);
@@ -380,6 +403,13 @@ int main(int argc, char** argv) {
 
         free(u);
         free(phi);
+    } else {
+        MPI_Waitall(last_req - 1, requests, MPI_STATUSES_IGNORE);
+
+        for (int i = 0; i < last_req; i++) {
+            free(send_buffers[i]);
+        }
+        free(send_buffers);
     }
 
     MPI_Finalize();
